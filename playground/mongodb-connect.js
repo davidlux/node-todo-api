5d193ab9fb29fea1190b9c4acd7753d1eaa9a84c
David Lux <david90lux@gmail.com>
2017-06-07 00:39:05 +0200

    Mongo Delete tests

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -13,6 +13,14 @@ MongoClient.connect(url, (err, db) => {
     return console.log('Unable to connect to MongoDB Server');
   }
 
+
+  // db.collection('Users').insertOne({
+  //   name: 'Mikayla Nietsch',
+  //   age: 24,
+  //   location: 'Ulm'
+  // })
+
+
   // db.collection('Todos').find({
   //   _id: new ObjectID('592ca79f5c2e63125c1ca0d7')
   // }).toArray().then((docs) => {
@@ -28,14 +36,40 @@ MongoClient.connect(url, (err, db) => {
   //   console.log('Unable to fetch docs', err);
   // });
 
+  // db.collection('Users').find({
+  //   name: 'David Lux'
+  // }).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // })
 
 
+  //delete many
+  // db.collection('Todos').deleteMany({
+  //   text: 'Eat lunch'
+  // }).then((result) => {
+  //   console.log(result);
+  // });
 
-  db.collection('Users').find({
+  //delete one
+  // db.collection('Todos').deleteOne({
+  //   text: 'Eat lunch'
+  // }).then(result => console.log(result));
+
+  //findone and delete
+  // db.collection('Todos').findOneAndDelete({
+  //   completed: false
+  // }).then(result => console.log(result));
+
+  db.collection('Users').deleteOne({
+    _id: ObjectID("592ca87074ca9d0de4521c5c")
+  }).then(result => console.log(result))
+
+  db.collection('Users').deleteMany({
     name: 'David Lux'
-  }).toArray().then((docs) => {
-    console.log(JSON.stringify(docs, undefined, 2));
-  })
+  }).then(result => console.log(result))
+
+
+
 
 
 
